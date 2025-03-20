1e726cca9494ea1ff8cde8efe3b607ad0e0d8ec4
jonathan <jpena@vector.com.mx>
2025-03-20 17:01:40 -0600

    Metricas de riesgo

--- Clases_ST/MCF_ST_clase_1.py
+++ Clases_ST/MCF_ST_clase_1.py
@@ -1,9 +1,10 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 import yfinance as yf
 import matplotlib.pyplot as plt
 import scipy.stats as stats
-from scipy.stats import kurtosis, skew, shapiro
+from scipy.stats import kurtosis, skew, shapiro ,norm
 
 st.title("Visualización de Rendimientos de Acciones")
 st.header("Streamlit clase 1 ")
@@ -80,6 +81,64 @@ if stock_seleccionado:
     stats.probplot(df_rendimientos[stock_seleccionado], dist="norm", plot=ax)
     ax.set_title("Q-Q Plot de los Rendimientos")
     st.pyplot(fig)
+    #Metricas de riesgo
+    # VaR Parametrico
+    mean = np.mean(df_rendimientos[stock_seleccionado])
+    stdev = np.std(df_rendimientos[stock_seleccionado])
+    VaR_95 = (norm.ppf(1-0.95,mean,stdev))
+
+    # Historical VaR
+    hVaR_95 = (df_rendimientos[stock_seleccionado].quantile(0.05))
+
+    # Monte Carlo
+
+    # Number of simulations
+    n_sims = 100000
+
+    # Simulate returns and sort
+    sim_returns = np.random.normal(mean, stdev, n_sims)
+
+    MCVaR_95 = np.percentile(sim_returns, 5)
+
+    CVaR_95 = (df_rendimientos[stock_seleccionado][df_rendimientos[stock_seleccionado] <= hVaR_95].mean())
+    st.subheader("Metricas de riesgo")
+    
+    col4, col5, col6, col7= st.columns(4)
+    col4.metric("95% VaR", f"{VaR_95:.4%}")
+    col5.metric("(Historical)", f"{hVaR_95:.4%}")
+    col6.metric("(Monte Carlo)", f"{MCVaR_95:.4%}")
+    col7.metric("95% CVaR", f"{CVaR_95:.4%}")
+
+    st.subheader("Grafica metricas de riesgo")
+
+    # Crear la figura y el eje
+    fig, ax = plt.subplots(figsize=(13, 5))
+
+    # Generar histograma
+    n, bins, patches = ax.hist(df_rendimientos[stock_seleccionado], bins=50, color='blue', alpha=0.7, label='Returns')
+
+    # Identificar y colorear de rojo las barras a la izquierda de hVaR_95
+    for bin_left, bin_right, patch in zip(bins, bins[1:], patches):
+        if bin_left < hVaR_95:
+            patch.set_facecolor('red')
+
+    # Marcar las líneas de VaR y CVaR
+    ax.axvline(x=VaR_95, color='skyblue', linestyle='--', label='VaR 95% (Paramétrico)')
+    ax.axvline(x=MCVaR_95, color='grey', linestyle='--', label='VaR 95% (Monte Carlo)')
+    ax.axvline(x=hVaR_95, color='green', linestyle='--', label='VaR 95% (Histórico)')
+    ax.axvline(x=CVaR_95, color='purple', linestyle='-.', label='CVaR 95%')
+
+    # Configurar etiquetas y leyenda
+    ax.set_title("Histograma de Rendimientos con VaR y CVaR")
+    ax.set_xlabel("Rendimiento Diario")
+    ax.set_ylabel("Frecuencia")
+    ax.legend()
+
+    # Mostrar la figura en Streamlit
+    st.pyplot(fig)
+
+
+
 
 
 
